e56836fcd7a2478692dfa2e7cc54b30ac9b49f0d
Joshua Lee <jstjoshin@users.noreply.github.com>
2025-06-16 16:31:10 -0500

    Initial project setup

---
+++ iOS-Capstone-Project/LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Joshua Lee on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
